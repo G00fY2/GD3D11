a205305082f5a457521edcf1d104e232a025b1ff
ThielHater <pierre.beckmann@yahoo.de>
2025-11-17 21:41:41 +0100

    Added externals to refresh settings and zen resources

--- D3D11Engine/DLLMain.cpp
+++ D3D11Engine/DLLMain.cpp
@@ -236,6 +236,7 @@ struct ddraw_dll {
     FARPROC	ReleaseDDThreadLock;
     FARPROC	UpdateCustomFontMultiplier;
     FARPROC	SetCustomSkyTexture;
+    FARPROC	LoadMenuSettings;
 } ddraw;
 
 HRESULT DoHookedDirectDrawCreateEx( GUID FAR* lpGuid, LPVOID* lplpDD, REFIID  iid, IUnknown FAR* pUnkOuter ) {
@@ -309,6 +310,14 @@ extern "C" void WINAPI SetCustomSkyWavelengths( float X, float Y, float Z ) {
     if ( sky ) {
         sky->SetCustomSkyWavelengths( X, Y, Z );
     }
+}
+
+extern "C" void WINAPI LoadMenuSettings() {
+    Engine::GAPI->LoadMenuSettings( MENU_SETTINGS_FILE );
+}
+
+extern "C" void WINAPI LoadCustomZENResources() {
+    Engine::GAPI->LoadCustomZENResources();
 }
 
 extern "C" void WINAPI EnableWindAnimations( void ) {
